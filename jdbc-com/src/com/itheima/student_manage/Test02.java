4a1bf51a776ac4c2b5fdfb041a932f1d520c3777
ccb <ccb@atguigu.com>
2024-04-06 10:25:09 +0800

    second commit

---
+++ jdbc-com/src/com/itheima/student_manage/Test02.java
@@ -0,0 +1,13 @@
+package com.itheima.student_manage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Test02 {
+    public static void main(String[] args) {
+        String name = null;
+        System.out.println("....");
+    }
+}
