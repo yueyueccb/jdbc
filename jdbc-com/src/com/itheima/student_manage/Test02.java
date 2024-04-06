8b78145b9f9a6432e5bf3ea783370d979a4fca84
ccb <ccb@atguigu.com>
2024-04-06 10:40:47 +0800

    hot-fix commit7

--- jdbc-com/src/com/itheima/student_manage/Test02.java
+++ jdbc-com/src/com/itheima/student_manage/Test02.java
@@ -9,6 +9,6 @@ public class Test02 {
     public static void main(String[] args) {
         String name = null;
         System.out.println("....");
-        System.out.println("...5");
+        System.out.println("master test");
     }
 }
